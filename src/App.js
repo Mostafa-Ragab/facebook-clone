5d0df879c02d22dadfaeb2824204f65b10397fb0
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-15 21:06:32 +0200

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './header/Header';
+import Sidebar from './sidebar/sidebar.component';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // BEM naming convention
+    <div className="App"> 
+      
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+      {/* feed */}
+      {/* widgets */}
     </div>
+      </div>
+      
   );
 }
 
