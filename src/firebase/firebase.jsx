fde686be2353281b88e3c80123c3d146465c5616
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-15 20:42:23 +0200

    added firebase

---
+++ src/firebase/firebase.jsx
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAhfXPN4As7ABKpQwQv0eJj6XF3c_eCb70",
+    authDomain: "facebook-clone-a367a.firebaseapp.com",
+    databaseURL: "https://facebook-clone-a367a.firebaseio.com",
+    projectId: "facebook-clone-a367a",
+    storageBucket: "facebook-clone-a367a.appspot.com",
+    messagingSenderId: "1085991472104",
+    appId: "1:1085991472104:web:acc1ac258230be75b3325d"
+  };
