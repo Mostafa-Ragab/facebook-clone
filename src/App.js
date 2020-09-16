70e706a1c064960f7418d75850e99a2a7ac5780c
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-16 19:21:16 +0200

    added widgets

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './App.css';
-import Header from './header/Header';
-import Sidebar from './sidebar/sidebar.component';
+import Header from './components/header/Header';
+import Sidebar from './components/sidebar/sidebar.component';
+import {useStateValue } from './components/contextApi/StateProvider';
+import Login from './components/login/Login';
+import Feed from './components/feed/Feed';
+import Widgets from './components/widgets/Widgets';
 function App() {
+  const [{user}, dispatch ] = useStateValue();
   return (
     // BEM naming convention
     <div className="App"> 
+      { !user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
       
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-      {/* feed */}
-      {/* widgets */}
     </div>
-      </div>
       
   );
 }
