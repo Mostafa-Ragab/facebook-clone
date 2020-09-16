22708dee5a33b808f0bc000fb65161c2720d04c1
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-16 19:24:08 +0200

    added contextapi to index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {StateProvider} from './components/contextApi/StateProvider'
 import * as serviceWorker from './serviceWorker';
+import reducer, {initialState} from './components/contextApi/reducer';
 
 ReactDOM.render(
   <React.StrictMode>
+  <StateProvider initialState={initialState} reducer={reducer}>
     <App />
+  </StateProvider> 
   </React.StrictMode>,
   document.getElementById('root')
 );
