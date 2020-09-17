5b793aa5f01ee7db9154edace602efc09e34519f
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-09-17 06:57:54 +0200

    added widgets components

--- src/components/widgets/Widgets.js
+++ src/components/widgets/Widgets.js
@@ -4,13 +4,13 @@ import './Widgets.css';
 function Widgets() {
     return (
         <div className="widgets">
-            <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FLiverpoolFC%2Fvideos%2F346115633437673%2F&show_text=0&width=340" 
+            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmostafa.rslan.9406%2Fposts%2F3201755959914647&width=340" 
             width="340" 
             height="100%" 
             style={{border:"none",overflow:"hidden"}}
             scrolling="no" 
-            frameborder="0" 
-            allowTransparency="true" 
+            frameBorder="0" 
+            allowtransparency="true" 
             allow="encrypted-media">
 
             </iframe>
